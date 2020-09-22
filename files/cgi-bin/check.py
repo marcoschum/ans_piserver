9946a1ef73633b6ec0f9b422d618ee8d6cb28ec3
mate <marcosch@id.ethz.ch>
2020-09-22 14:33:25 +0200

    updated temperature reading in status.php page

--- files/cgi-bin/check.py
+++
@@ -1,3 +0,0 @@
-import os
-
-print(os.get_size(/))
--- files/html/status.php
+++ files/html/status.php
@@ -5,7 +5,18 @@
     $base = 1024;
     $class_root = min((int)log($bytes_root , $base) , count($si_prefix) - 1);
     $class_disk = min((int)log($bytes_disk , $base) , count($si_prefix) - 1);
+		$output_cpu = shell_exec("/bin/cat /sys/class/thermal/thermal_zone0/temp");
+		$output_gpu = shell_exec("/opt/vc/bin/vcgencmd measure_temp");
+
+
+		echo "<center>";
     echo "Free disk space on '/' : " . sprintf('%1.2f' , $bytes_root / pow($base,$class_root)) . ' ' . $si_prefix[$class_root] . '<br />';
     echo "Free disk space on '/data' : " . sprintf('%1.2f' , $bytes_disk / pow($base,$class_disk)) . ' ' . $si_prefix[$class_disk] . '<br />';
 
+
+    echo "CPU Temp: " . ($output_cpu / 1000) . " °C<br>";
+    echo "GPU Temp: $output_gpu";
+
+    echo "<button onClick=\"window.location.reload();\">reload</button>";
+		echo "</center>";
 ?>
